b30906bc0135eb5aa73c2543993ae94db34bd94a
John Worley <john.aver.worley@gmail.com>
2018-12-15 12:49:02 -0600

    changePage button in admin

--- src/components/AdminComponent/AdminComponent.jsx
+++ src/components/AdminComponent/AdminComponent.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom'
 
 
 class Admin extends Component {
+    changePage = () => {
+        this.props.history.push('/') // back to our main page
+    }
+
     // Renders the entire app on the DOM
     render() {
         return (
             <div className="Admin">
                 <p>admin page</p>
+                <button onClick={this.changePage}>Back to Project View</button> 
             </div>
         );
     }
 }
 
-export default connect()(Admin);
+export default withRouter(connect()(Admin));
